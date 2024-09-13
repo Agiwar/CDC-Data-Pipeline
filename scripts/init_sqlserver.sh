54e1fa34bd23ae4fd2a8205112bc745bc04595d2
Agiwar <agiwar791005@gmail.com>
2024-09-13 19:39:40 +0800

    add a condition for waiting for sql server service all up

--- scripts/init_sqlserver.sh
+++ scripts/init_sqlserver.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-sleep 30s
+# Wait for SQL Server to be available
+echo "Waiting for SQL Server to start..."
+until /opt/mssql-tools/bin/sqlcmd -S sqlserver -U sa -P 'Password!' -Q "SELECT 1" &> /dev/null; do
+    echo "SQL Server is not ready yet. Sleeping for 5 seconds..."
+    sleep 5
+done
+
+echo "SQL Server is up. Running initialization script..."
 /opt/mssql-tools/bin/sqlcmd -S sqlserver -U sa -P 'Password!' -d master -i init_PurchaseMain.sql
